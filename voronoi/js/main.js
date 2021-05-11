bdc93fe330c0b9b24af6d0aba42c9532238659fa
pthiemt <peggy.thiemt@tu-dresden.de>
2021-05-11 17:48:29 +0200

    adding input for number of randomly generated points + fit canvas to available size

--- voronoi/js/main.js
+++ voronoi/js/main.js
@@ -1,22 +1,35 @@
 $(function () {
-	$('button[name="showhelp"]').click(function(){
-		var w = window.open("html/helpwindow.html", "popupWindow", "width=600, height=400, scrollbars=yes");
-	});
-
-	// -----------------------------------------------------------
-	// language switch
-	// -----------------------------------------------------------
+	
+	
+	var canvas = $("#voronoiCanvas");
+	
+	Voronoi.init();	
+	Voronoi.setCanvasSize(canvas.parent().width(),canvas.parent().height());
+	Voronoi.clearSites();
+	Voronoi.generateSites(100);
 
 	// default lang is German => hide all English elements
 	$('[lang="en"]').hide();
 	$('button[name="de"]').prop("disabled", true);
+
+
+	$("#fpsswitcher").on('input', function() {
+
+		$("#fps").html(parseInt($("#fpsswitcher").val()));
+		Voronoi.clearSites();
+		Voronoi.generateSites(parseInt($("#fpsswitcher").val()));
+	});
+
 
 	// add click handler to all elements of type button
 	$("button").click(function () {
 		// get name of button
 		var clicked = $(this).attr("name");
 
-		if (clicked.localeCompare("en") == 0) {
+		if (clicked.localeCompare("resetClear") == 0) {
+			Voronoi.clearSites();
+		}
+		else if (clicked.localeCompare("en") == 0) {
 			$('[lang="en"]').show();
 			$('[lang="de"]').hide();
 			$('button[name="en"]').prop("disabled", true);
